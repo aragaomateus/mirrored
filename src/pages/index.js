a0f2ae2220d1df66df45df01b702f22832aa32d5
Mateus Silva Aragao <msa8779@nyu.edu>
2023-12-28 12:48:39 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-spotify-black text-spotify-white">
+      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+        <h1 className="text-6xl font-bold mb-6">Welcome to Mirrored</h1>
+
+        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6">
+          <Link href="/generate-opposite-playlist" passHref>
+            <div className="p-6 mt-6 text-left border border-spotify-green w-96 rounded-xl hover:text-spotify-black hover:bg-spotify-green cursor-pointer">
+              <h2 className="text-2xl">Generate Opposite Playlist &rarr;</h2>
+            </div>
+          </Link>
+
+          <Link href="/find-sound-similarity" passHref>
+            <div className="p-6 mt-6 text-left border border-spotify-green w-96 rounded-xl hover:text-spotify-black hover:bg-spotify-green cursor-pointer">
+              <h2 className="text-2xl">Find Out Sound Similarity &rarr;</h2>
+            </div>
+          </Link>
+        </div>
+      </main>
+    </div>
+  );
+}
+
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      colors: {
+        'spotify-green': '#1DB954',
+        'spotify-black': '#191414',
+        'spotify-white': '#ffffff',
+      },
+    },
+  },
+  plugins: [],
+}
